c7e0db0ef62717a8c903b54a406d3bdf26bbb41d
Louison <louison.boisselier@gmail.com>
2020-12-01 13:04:11 +0100

    add routes adresses and modification user table

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_01_100759) do
+ActiveRecord::Schema.define(version: 2020_12_01_114254) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "adresses", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.integer "street_number"
+    t.string "street"
+    t.string "zip_code"
+    t.string "country"
+    t.string "phone_number"
+    t.string "company_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "company_name"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
