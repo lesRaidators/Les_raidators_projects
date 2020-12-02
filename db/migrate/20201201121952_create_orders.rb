5a2b8315f6e366e0bd1d06defa0ee6d09d3e110d
Lola <lolaleandri@protonmail.com>
2020-12-02 11:58:18 +0100

    add migration

--- db/migrate/20201201121952_create_orders.rb
+++ db/migrate/20201201121952_create_orders.rb
@@ -2,11 +2,7 @@ class CreateOrders < ActiveRecord::Migration[5.2]
   def change
     create_table :orders do |t|
       t.string :stripe_customer_id
-<<<<<<< HEAD
   
-=======
-      t.references :user, null: false, foreign_key: true
->>>>>>> development
       t.timestamps
     end
   end
