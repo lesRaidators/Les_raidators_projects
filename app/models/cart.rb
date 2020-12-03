7ec6d644f58d0db355af2eba13a6c291cd214bc3
Lola <lolaleandri@protonmail.com>
2020-12-03 12:39:38 +0100

    creation controller cart + modification SP and P

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -2,4 +2,14 @@ class Cart < ApplicationRecord
   belongs_to :user
   has_many :selected_products
   
+  def add_product(product)
+    product = selected_products.find_by(product_id: product)
+    if product
+      product.quantity += 1
+    else
+      product = selected_products.new(product: product)
+    end
+    product
+  end
+
 end
