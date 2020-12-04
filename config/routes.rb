4cc7c345bd97ad42fad9efb81c43ef09f8ca58e1
Emilie <emilie.aussage@gmail.com>
2020-12-04 17:24:09 +0100

    add last changes to push development

--- config/routes.rb
+++ config/routes.rb
@@ -3,14 +3,9 @@ Rails.application.routes.draw do
   get '/partnership', to: "static_pages#partnership"
   get '/home_page', to: "static_pages#home_page"
   get '/team', to: "static_pages#team"
-<<<<<<< HEAD
-  get 'privatevisitors/show'
-  
-=======
   get '/privatevisitors', to: "static_pages#privatevisitors"
   get '/equipments', to:"static_pages#equipments"
 
->>>>>>> development
   devise_for :users
   resources :adresses
   resources :selected_products
