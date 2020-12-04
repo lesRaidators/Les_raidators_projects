4100fce97d3ae029c970391ee5b17fcb4c3ca284
Louison <louison.boisselier@gmail.com>
2020-12-04 16:41:05 +0100

    add migration admin

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_01_123030) do
+ActiveRecord::Schema.define(version: 2020_12_04_153419) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 2020_12_01_123030) do
 
   create_table "carts", force: :cascade do |t|
     t.bigint "user_id", null: false
+    t.decimal "stripe_price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_carts_on_user_id"
@@ -42,6 +43,7 @@ ActiveRecord::Schema.define(version: 2020_12_01_123030) do
   create_table "join_order_products", force: :cascade do |t|
     t.bigint "order_id", null: false
     t.bigint "product_id", null: false
+    t.integer "quantity"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["order_id"], name: "index_join_order_products_on_order_id"
@@ -51,6 +53,7 @@ ActiveRecord::Schema.define(version: 2020_12_01_123030) do
   create_table "orders", force: :cascade do |t|
     t.string "stripe_customer_id"
     t.bigint "user_id", null: false
+    t.decimal "total_price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_orders_on_user_id"
@@ -66,7 +69,7 @@ ActiveRecord::Schema.define(version: 2020_12_01_123030) do
   end
 
   create_table "selected_products", force: :cascade do |t|
-    t.integer "quantity"
+    t.integer "quantity", default: 1
     t.bigint "cart_id", null: false
     t.bigint "product_id", null: false
     t.datetime "created_at", null: false
@@ -86,6 +89,7 @@ ActiveRecord::Schema.define(version: 2020_12_01_123030) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
