9e61e625e20a5034643d8ad2169c6e6fce915eb3
Louison <louison.boisselier@gmail.com>
2020-12-07 15:04:37 +0100

    add feature Post

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,7 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 Product.destroy_all
+Post.destroy_all
 
 
 Product.create(
@@ -33,3 +34,12 @@ Product.create(
 )
 puts "don de 30€"
 
+10.times do
+Post.create(
+  title: "Les aventures d'Emilie",
+  content: "Nous voilà arriver à .............. C'est chouette.... ",
+  image_url: "https://images.unsplash.com/photo-1518101645466-7795885ff8f8?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1350&q=80",
+  user_id: 1
+)
+puts "articles du blog"
+end
