edb3ff1556d00b268ec79e4db3a73f5aba99f679
Louison <louison.boisselier@gmail.com>
2020-12-08 18:15:42 +0100

    fix conflict in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -20,13 +20,10 @@ Rails.application.routes.draw do
   end 
   root 'static_pages#home_page'
   
-<<<<<<< HEAD
   scope '/checkout' do
     post 'create', to: 'checkout#create', as: 'checkout_create'
     get 'cancel', to: 'checkout#cancel', as: 'checkout_cancel'
     get 'success', to: 'checkout#success', as: 'checkout_success'
   end
   
-=======
->>>>>>> development
 end
