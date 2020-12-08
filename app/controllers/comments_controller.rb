73aeb95e073a7119aed55f991cd583c0de35f749
Lola <lolaleandri@protonmail.com>
2020-12-08 09:51:09 +0100

    comment add but need formatting

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,10 @@
 class CommentsController < ApplicationController
   def index
-  
+  end
   def create 
     @post = Post.find(params[:post_id])
-    @comment = @post.comments.create(params[:comment].permit(:body).merge(user_id: current_user.id))
+    @comment = @post.comments.create(comment_params.merge(user_id: current_user.id))
+    redirect_to post_path(@post)
   end
 
   def destroy
