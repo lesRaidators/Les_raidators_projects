91bae6be7d2586caa5503f6035d513d2e07f9462
Lola <lolaleandri@protonmail.com>
2020-12-08 10:36:51 +0100

    fix bug of schema and seeds/ readd video

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,7 @@ ActiveRecord::Schema.define(version: 2020_12_07_150754) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "adresses", force: :cascade do |t|
+  create_table "addresses", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
     t.integer "street_number"
@@ -25,12 +25,16 @@ ActiveRecord::Schema.define(version: 2020_12_07_150754) do
     t.string "country"
     t.string "phone_number"
     t.string "company_name"
+    t.integer "status", default: 0
+    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_addresses_on_user_id"
   end
 
   create_table "carts", force: :cascade do |t|
     t.bigint "user_id", null: false
+    t.decimal "stripe_price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_carts_on_user_id"
@@ -49,6 +53,7 @@ ActiveRecord::Schema.define(version: 2020_12_07_150754) do
   create_table "join_order_products", force: :cascade do |t|
     t.bigint "order_id", null: false
     t.bigint "product_id", null: false
+    t.integer "quantity"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["order_id"], name: "index_join_order_products_on_order_id"
@@ -58,6 +63,7 @@ ActiveRecord::Schema.define(version: 2020_12_07_150754) do
   create_table "orders", force: :cascade do |t|
     t.string "stripe_customer_id"
     t.bigint "user_id", null: false
+    t.decimal "total_price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_orders_on_user_id"
@@ -84,7 +90,7 @@ ActiveRecord::Schema.define(version: 2020_12_07_150754) do
   end
 
   create_table "selected_products", force: :cascade do |t|
-    t.integer "quantity"
+    t.integer "quantity", default: 1
     t.bigint "cart_id", null: false
     t.bigint "product_id", null: false
     t.datetime "created_at", null: false
@@ -97,18 +103,19 @@ ActiveRecord::Schema.define(version: 2020_12_07_150754) do
     t.string "first_name"
     t.string "last_name"
     t.string "company_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "addresses", "users"
   add_foreign_key "carts", "users"
   add_foreign_key "comments", "posts"
   add_foreign_key "comments", "users"
