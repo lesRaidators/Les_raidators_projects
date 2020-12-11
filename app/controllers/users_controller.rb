eb440d9e8806fd5a0fc54fc1f81cf7227ba2a2f8
Emma <emmalg.work@gmail.com>
2020-12-11 12:51:14 +0100

    addresses

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,5 +6,6 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @orders = Order.where(user_id: @user.id).includes(:join_order_products, :products)
+    @address = Address.find(params[:id])
   end
 end
