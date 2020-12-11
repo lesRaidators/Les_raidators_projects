4936baf8d4c46f33fae40d6b66b25e3307566863
Lola <lolaleandri@protonmail.com>
2020-12-11 09:38:49 +0100

    clear devise views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,15 @@
 class ApplicationController < ActionController::Base
 
-  protect_from_forgery with: :exception
+  protect_from_forgery with: :exception 
   before_action :configure_permitted_params, if: :devise_controller?
- 
-
-
 
   private
   def configure_permitted_params
+
     devise_parameter_sanitizer.permit(:sign_up) {|u| u.permit(:email, :password)}
     devise_parameter_sanitizer.permit(:account_update) {|u| u.permit(:email, :password, :current_password)}
+    
   end 
+  
+
 end
