0c5853c58fdfd8ee883260d8d5dcd698c9fe4794
Louison <louison.boisselier@gmail.com>
2020-12-11 13:31:59 +0100

    change path address in profil

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
+    @user = current_user
     @address= Address.find(params[:id])
     @orders = Order.where(user_id: @user.id).includes(:join_order_products, :products)
   end
